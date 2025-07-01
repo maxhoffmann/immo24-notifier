902fbb9c6a7c09e87770bd52dbe09ed568d84e32
Max Hoffmann <hey@max.hn>
2025-07-01 11:41:50 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("button").addEventListener("click", () => {
-    let options = {
+    chrome.notifications.create({
       type: "basic",
       title: "x neue Anzeigen!",
       message: "auf ImmoScout24",
       iconUrl: "icon-48.png",
-    };
-    chrome.notifications.create(options);
+    });
   });
 });
